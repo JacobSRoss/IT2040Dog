567dfcf8a9b88ead03a3346495d37d95f873d69c
Jacob Ross <jross1818@gmail.com>
2018-09-13 20:29:17 -0500

    possible finish for assignment

--- DogChallenge/Program.cs
+++ DogChallenge/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace DogChallenge
 {
+    enum Gender { Male, Female };
+
+    class Dog {
+        public Dog(String name, String owner, int age, Gender gender){
+            name = this.name;
+            owner = this.owner;
+            age = this.age;
+            gender = this.gender;
+        }
+        public String name;
+        public String owner;
+        public int age;
+        public Gender gender;
+
+        public void bark (int barks) {
+            for (int i=0; i<barks; i++){
+                Console.WriteLine("Woof!");
+            }
+        }
+
+        public String getTag (){
+            String tag = "";
+            if(gender == Gender.Male && age <= 1) {
+                tag = "If lost call " + owner +" His name is " + name + " and he is " + age + " year old.";
+            } else if(gender == Gender.Male && age > 1) {
+                tag = "His name is " + name + " and he is " + age + " years old.";
+            } else if(gender == Gender.Female && age <= 1) {
+                tag = "Her name is " + name + " and she is " + age + " year old.";
+            } else if(gender == Gender.Female && age > 1) {
+                tag = "Her name is " + name + " and she is " + age + " years old.";
+            }
+
+            return tag;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);  // create object instance
+            puppy.bark(3); // output: Woof!Woof!Woof!
+            Console.WriteLine(puppy.getTag()); // output: If lost, call Shawn. His name is Orion and he is 1 year old.
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);  // create object instance
+            myDog.bark(1); // output: Woof!
+            Console.WriteLine(myDog.getTag()); // output: If lost, call Dale. Her name is Lileu and she is 4 years old.
+            Console.ReadLine();
         }
     }
 }
